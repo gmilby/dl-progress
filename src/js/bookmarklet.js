a71810ec3d433da7c4729b6068a8b1ce75f459b3
Rich Knight <rich@rich-knight.co.uk>
2012-11-24 22:47:34 +0000

    bookmarklet js to load jquery if needed

---
+++ src/js/bookmarklet.js
@@ -0,0 +1,24 @@
+/*
+ * jQuery Bookmarklet - version 1.0
+ * Originally written by: Brett Barros
+ * With modifications by: Paul Irish
+ *
+ * If you use this script, please link back to the source
+ *
+ * Copyright (c) 2010 Latent Motion (http://latentmotion.com/how-to-create-a-jquery-bookmarklet/)
+ * Released under the Creative Commons Attribution 3.0 Unported License,
+ * as defined here: http://creativecommons.org/licenses/by/3.0/
+ *
+ */
+ 
+window.bookmarklet = function(opts){fullFunc(opts)};
+ 
+//this script loads jquery first if needed, then the progress script
+window.bookmarklet({
+    css : [],
+    js  : ['https://raw.github.com/rcknight/progress.js/master/src/js/progress.js'],    
+//	jqpath : 'myCustomjQueryPath.js', <-- option to include your own path to jquery
+    ready : function(){}
+})
+ 
+function fullFunc(a){function d(b){if(b.length===0){a.ready();return false}$.getScript(b[0],function(){d(b.slice(1))})}function e(b){$.each(b,function(c,f){$("<link>").attr({href:f,rel:"stylesheet"}).appendTo("head")})}a.jqpath=a.jqpath||"https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js";(function(b){var c=document.createElement("script");c.type="text/javascript";c.src=b;c.onload=function(){e(a.css);d(a.js)};document.body.appendChild(c)})(a.jqpath)};
